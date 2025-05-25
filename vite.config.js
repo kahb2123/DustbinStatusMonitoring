dac13e38769353d9fc5e45711edb77a45f650ef0
kahb2123 <kassahunassabe@gmail.com>
2025-05-26 01:53:40 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+   base: '/DustbinStatusMonitoring/', // 
+  build: {
+    outDir: 'dist', // Ensure build goes to 'dist'
+  },
   plugins: [react()],
   resolve: {
     alias: {
